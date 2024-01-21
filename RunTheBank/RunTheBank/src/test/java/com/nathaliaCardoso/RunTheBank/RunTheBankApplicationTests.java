30815da5d44e38519d15c456d6b3333461cb6cb6
nathaliafcardoso <nathaliaf.cardoso@outlook.com>
2024-01-21 15:50:47 -0300

    Init commit and config H2

---
+++ RunTheBank/RunTheBank/src/test/java/com/nathaliaCardoso/RunTheBank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathaliaCardoso.RunTheBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
